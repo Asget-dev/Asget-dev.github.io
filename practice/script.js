2e405cb65f48d0522e701baf5e7efc02450866f0
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-14 23:12:18 -0700

    Update script.js

--- practice/script.js
+++ practice/script.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 
 
-function tipCalculater(){
+function swap(){
     var tmp = document.getElementById("c").value;
     document.getElementById("c").value = document.getElementById("d").value;
     document.getElementById("d").value = tmp;
 }
 window.onload=()=>{
-    document.getElementById("go").onclick = tipCalculater;
+    document.getElementById("go").onclick = swap;
 }
