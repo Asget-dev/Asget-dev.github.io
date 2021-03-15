cbaa6bec2bf9283a13d61f0b91551b0186acda37
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-14 23:40:14 -0700

    swap

--- practice/script.js
+++ practice/script.js
@@ -3,8 +3,11 @@
 
 function swap(){
     var tmp = document.getElementById("c").value;
-    document.getElementById("c").value = document.getElementById("d").value;
-    document.getElementById("d").value = tmp;
+    document.getElementById("c").value = document.getElementById("output").innerHTML;
+    let x=document.getElementById("output").innerHTML;
+    x = tmp;
+    let color=x.fontcolor("green");
+    document.getElementById("output").innerHTML = color ;
 }
 window.onload=()=>{
     document.getElementById("go").onclick = swap;
