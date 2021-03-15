002b26450872971fa99239027da646aedade8bcc
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-03-15 10:02:57 -0700

    Update script.js

--- practice/script.js
+++ practice/script.js
@@ -4,11 +4,13 @@
 function swap(){
     var tmp = document.getElementById("c").value;
     document.getElementById("c").value = document.getElementById("output").innerHTML;
-    let x=document.getElementById("output").innerHTML;
-    x = tmp;
-    let color=x.fontcolor("green");
-    document.getElementById("output").innerHTML = color ;
+    document.getElementById("output").innerHTML = tmp;
+   
+   let j = document.getElementById("output").innerHTML;
+   let color=j.fontcolor("green");
+   document.getElementById("output").innerHTML = color ;
 }
 window.onload=()=>{
     document.getElementById("go").onclick = swap;
 }
+
